71cc10bbcbb3340a8a7415fbe89922048635bd1e
sebach1 <sebachamena@gmail.com>
2019-10-06 15:23:49 -0300

    Add static value checkers

--- git/change.go
+++ git/change.go
@@ -1,6 +1,12 @@
 package git
 
-import "github.com/sebach1/git-crud/schema"
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+
+	"github.com/sebach1/git-crud/schema"
+)
 
 // A Change represents every purposed difference
 type Change struct {
@@ -9,9 +15,107 @@ type Change struct {
 	TableName  schema.TableName  `json:"table_name,omitempty"`
 	ColumnName schema.ColumnName `json:"column_name,omitempty"`
 
-	Value schema.Value `json:"value,omitempty"`
+	StrValue     string          `json:"str_value,omitempty"`
+	IntValue     int             `json:"int_value,omitempty"`
+	Float32Value float32         `json:"float32_value,omitempty"`
+	Float64Value float64         `json:"float64_value,omitempty"`
+	JSONValue    json.RawMessage `json:"json_value,omitempty"`
+	BytesValue   bytes.Buffer    `json:"bytes_value,omitempty"`
 
 	EntityID schema.ID `json:"entity_id,omitempty"`
+
+	Type string
+}
+
+// Value gives an interface handling the real value
+// Used to perform comparisons
+func (chg *Change) Value() interface{} {
+	switch chg.Type {
+	case "string":
+		return chg.StrValue
+	case "int":
+		return chg.IntValue
+	case "float32":
+		return chg.Float32Value
+	case "float64":
+		return chg.Float64Value
+	case "json":
+		return chg.JSONValue
+	case "bytes":
+		return chg.BytesValue
+	}
+	return nil
+}
+
+func (chg *Change) tearDownValue() {
+	defer func() { chg.Type = "" }()
+	switch chg.Type {
+	case "string":
+		chg.StrValue = ""
+
+	case "int":
+		chg.IntValue = 0
+	case "float32":
+		chg.Float32Value = 0
+	case "float64":
+		chg.Float64Value = 0
+
+	case "json":
+		chg.JSONValue = json.RawMessage{}
+	case "bytes":
+		chg.BytesValue = bytes.Buffer{}
+	}
+}
+
+// SetValue performs type assertion over the given value and sets the value over the given change
+// In case of failure on all the possible type assertions, returns an error
+// Notice that SetValue will ALWAYS tearDown the value set up before
+func (chg *Change) SetValue(val interface{}) (err error) {
+	chg.tearDownValue()
+
+	if strVal, ok := val.(string); ok {
+		chg.StrValue = strVal
+		chg.Type = "string"
+		return
+	}
+	if intVal, ok := val.(int); ok {
+		chg.IntValue = intVal
+		chg.Type = "int"
+		return
+	}
+	if f32Val, ok := val.(float32); ok {
+		chg.Float32Value = f32Val
+		chg.Type = "float32"
+		return
+	}
+	if f64Val, ok := val.(float64); ok {
+		chg.Float64Value = f64Val
+		chg.Type = "float64"
+		return
+	}
+	if jsVal, ok := val.(json.RawMessage); ok {
+		chg.JSONValue = jsVal
+		chg.Type = "json"
+		return
+	}
+	if byBuff, ok := val.(bytes.Buffer); ok {
+		chg.BytesValue = byBuff
+		chg.Type = "bytes"
+		return
+	}
+
+	return errors.New("the given value cannot be safety typed")
+}
+
+// Validate self
+func (chg *Change) Validate() error {
+	if chg.TableName == "" {
+		return errZeroTable
+	}
+	if chg.ColumnName == "" {
+		return errZeroColumn
+	}
+	return nil
 }
 
 // IsUntracked retrieves true if the change is a new entity, otherwise returns false
@@ -20,6 +124,22 @@ func (chg *Change) IsUntracked() bool {
 		return true
 	}
 	return false
+}
+
+// Equals checks if the given change will trigger the exactly same action as itself
+// That is: the change is not untracked, the column and table affected are the same
+// and the value is the same
+func (chg *Change) Equals(otherChg *Change) bool {
+	if !IsCompatibleWith(chg, otherChg) {
+		return false
+	}
+	if chg.ColumnName != otherChg.ColumnName {
+		return false
+	}
+	if chg.Value() != otherChg.Value() {
+		return false
+	}
+	return true
 }
 
 // IsCompatibleWith checks if the changes belongs to the same table and the same entity
--- git/change_test.go
+++ git/change_test.go
@@ -1,6 +1,7 @@
 package git
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -87,6 +88,119 @@ func TestIsCompatibleWith(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := IsCompatibleWith(tt.args.chg, tt.args.otherChg); got != tt.want {
 				t.Errorf("IsCompatibleWith() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestChange_SetValue(t *testing.T) {
+	type args struct {
+		val interface{}
+	}
+	cleansedChgs := []*Change{gChanges.Regular.CleanValue, gChanges.Rare.CleanValue}
+	tests := []struct {
+		name     string
+		chg      *Change
+		args     args
+		wantErr  bool
+		wantType string
+	}{
+		{
+			name:     "string",
+			chg:      randChg(cleansedChgs...),
+			args:     args{val: gChanges.Regular.StrValue.StrValue},
+			wantErr:  false,
+			wantType: "string",
+		},
+		{
+			name:     "json",
+			chg:      randChg(cleansedChgs...),
+			args:     args{val: gChanges.Regular.JSONValue.JSONValue},
+			wantErr:  false,
+			wantType: "json",
+		},
+		{
+			name:     "int",
+			chg:      randChg(cleansedChgs...),
+			args:     args{val: gChanges.Regular.IntValue.IntValue},
+			wantErr:  false,
+			wantType: "int",
+		},
+		{
+			name:     "float32",
+			chg:      randChg(cleansedChgs...),
+			args:     args{val: gChanges.Regular.Float32Value.Float32Value},
+			wantErr:  false,
+			wantType: "float32",
+		},
+		{
+			name:     "float64",
+			chg:      randChg(cleansedChgs...),
+			args:     args{val: gChanges.Regular.Float64Value.Float64Value},
+			wantErr:  false,
+			wantType: "float64",
+		},
+		{
+			name:     "nil",
+			chg:      randChg(cleansedChgs...),
+			args:     args{val: nil},
+			wantErr:  true,
+			wantType: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.chg.SetValue(tt.args.val); (err != nil) != tt.wantErr {
+				t.Errorf("Change.SetValue() error got: %v, wantErr %v", err, tt.wantErr)
+			}
+			if gotType := tt.chg.Type; gotType != tt.wantType {
+				t.Errorf("Change.SetValue() type saved mismatch; got: %v, want: %v", gotType, tt.wantType)
+			}
+		})
+	}
+}
+
+func TestChange_Value(t *testing.T) {
+	tests := []struct {
+		name string
+		chg  *Change
+		want interface{}
+	}{
+		{
+			name: "json",
+			chg:  gChanges.Regular.JSONValue,
+			want: gChanges.Regular.JSONValue.JSONValue,
+		},
+		{
+			name: "str",
+			chg:  gChanges.Regular.None,
+			want: gChanges.Regular.None.StrValue,
+		},
+		{
+			name: "float32",
+			chg:  gChanges.Regular.Float32Value,
+			want: gChanges.Regular.Float32Value.Float32Value,
+		},
+		{
+			name: "int",
+			chg:  gChanges.Regular.IntValue,
+			want: gChanges.Regular.IntValue.IntValue,
+		},
+		{
+			name: "float64",
+			chg:  gChanges.Regular.Float64Value,
+			want: gChanges.Regular.Float64Value.Float64Value,
+		},
+		{
+			name: "str",
+			chg:  gChanges.Regular.None,
+			want: gChanges.Regular.None.StrValue,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.chg.Value(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Change.Value() = %v, want %v", got, tt.want)
 			}
 		})
 	}
