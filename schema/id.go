5000618206651bded13cc0116e86e600c82a50e8
sebach1 <sebachamena@gmail.com>
2019-10-06 15:25:24 -0300

    Add id nil checker; stringifies ID

---
+++ schema/id.go
@@ -0,0 +1,12 @@
+package schema
+
+// ID is a string in order to handle with every kind of id types (UUID/GUID/int)
+type ID string
+
+// IsNil verifies if the id is zero-valued
+func (id ID) IsNil() bool {
+	if id == "0" || id == "" {
+		return true
+	}
+	return false
+}
