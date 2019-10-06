763b33a91ea69a15a98da57f3a378b8ca898263c
sebach1 <sebachamena@gmail.com>
2019-10-06 15:23:09 -0300

    Adapt store to pkg changes

--- cmd/store/store.go
+++ cmd/store/store.go
@@ -2,22 +2,26 @@ package store
 
 import (
 	"context"
+
+	"github.com/sebach1/git-crud/git"
+	"github.com/sebach1/git-crud/schema"
 )
 
 // The Store abstracts every CRUD app
 type Store interface {
 
 	// PushSingle will override a single value on the specified field
-	PushSingle(context.Context, schematypes.Value, schematypes.ID, schematypes.TableName, schematypes.ColumnName) error
+	// Its a shortcut for
+	PushSingle(context.Context, schema.Value, schema.ID, schema.TableName, schema.ColumnName) error
 
 	// Push will post the given commit. It returns a slice of IDs
-	Push(context.Context, *schematypes.Commit) (*schematypes.Summary, error)
+	Push(context.Context, *git.Commit) (*git.Summary, error)
 
 	// Pull retrieves the column(s) given from an entity on a table
-	Pull(context.Context, schematypes.ID, schematypes.TableName, ...schematypes.ColumnName) (*schematypes.Commit, error)
+	Pull(context.Context, schema.ID, schema.TableName, ...schema.ColumnName) (*git.Commit, error)
 
 	// Remove deletes a record given its id and table
-	Remove(context.Context, schematypes.ID, schematypes.TableName) error
+	Remove(context.Context, schema.ID, schema.TableName) error
 
 	// Init initializes every persistant needed action (such as token refreshing)
 	Init(context.Context) error
