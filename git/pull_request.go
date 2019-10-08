1ab9b797cefdf6c8e3b99e79032a467cacdfe9a3
Sebastián Chamena <sebachamena@gmail.com>
2019-10-08 17:17:21 -0300

    Add pR micro-orchestration

--- git/pull_request.go
+++ git/pull_request.go
@@ -1,7 +1,18 @@
 package git
 
+import "github.com/sebach1/git-crud/internal/integrity"
+
 type PullRequest struct {
 	ID      int
-	Name    string
+	Team    *Team
 	Commits []*Commit
+}
+
+func (pR *PullRequest) AssignTeam(community *Community, schName integrity.SchemaName) error {
+	team, err := community.LookFor(schName)
+	if err != nil {
+		return err
+	}
+	pR.Team = team
+	return nil
 }
