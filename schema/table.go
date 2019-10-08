ee4f90067ab2aa822ba9eb4b21c2eaa84c4894d9
sebach1 <sebachamena@gmail.com>
2019-10-07 23:19:58 -0300

    Add Maintainer after restructuration

--- schema/table.go
+++ schema/table.go
@@ -1,14 +1,15 @@
 package schema
 
 import (
+	"github.com/sebach1/git-crud/git"
 	"github.com/sebach1/git-crud/internal/integrity"
 )
 
 // A Table is the representation of SQL table (or Mongo/CQL Collections) which acts as a collection of entities.
 type Table struct {
-	Name    integrity.TableName `json:"name,omitempty"`
-	Columns []*Column           `json:"columns,omitempty"`
-	// Maintainer git.Collaborator `json:"maintainer,omitempty"`
+	Name       integrity.TableName `json:"name,omitempty"`
+	Columns    []*Column           `json:"columns,omitempty"`
+	Maintainer git.Collaborator    `json:"maintainer,omitempty"`
 }
 
 func (t *Table) columnNames() (colNames []integrity.ColumnName) {
