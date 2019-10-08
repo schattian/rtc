d122dc3d6a22949475805b722d0cdda4f9e657bf
sebach1 <sebachamena@gmail.com>
2019-10-07 23:19:16 -0300

    Add CRUD errs; Normalize

--- git/errors.go
+++ git/errors.go
@@ -4,7 +4,17 @@ import "errors"
 
 var (
 	// Changes
-	errDuplicatedChg = errors.New("the change is ALREADY COMMITED")
-	errZeroTable     = errors.New("change's TABLE cannot be ZERO")
-	errZeroColumn    = errors.New("change's COLUMN cannot be ZERO")
+	errDuplicatedChg     = errors.New("the change is ALREADY COMMITED")
+	errUnclassifiableChg = errors.New("the change DOESNT respect any PATTERN and thus CANNOT be CLASSIFIABLE")
+
+	errNilTable = errors.New("change's TABLE cannot be NIL")
+
+	errNotNilColumn = errors.New("change's COLUMN cannot be NOT NIL")
+	errNilColumn    = errors.New("change's COLUMN cannot be NIL")
+
+	errNilEntityID    = errors.New("the ENTITY_ID is NIL")
+	errNotNilEntityID = errors.New("the ENTITY_ID is NOT NIL")
+
+	errNilValue    = errors.New("the VALUE cannot be NIL")
+	errNotNilValue = errors.New("the VALUE cannot be NOT NIL")
 )
