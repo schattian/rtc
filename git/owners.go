1fce52a65e85ca6a0b1f432afb1aadec39ad88c1
sebach1 <sebachamena@gmail.com>
2019-10-07 23:17:42 -0300

    Add Owner orchestration

--- git/owners.go
+++ git/owners.go
@@ -6,18 +6,32 @@ type Owner struct {
 
 // Orchestrate sends the order to all the collaborators to execute
 // the needed actions in order to achieve the commitment
-func (o *Owner) Orchestrate(comm *Commit) {
-	comm.GroupBy(AreCompatible)
+func (o *Owner) Orchestrate(comm *Commit, name string, strategy changesMatcher) {
+	pR := PullRequest{Name: name}
+	for _, changes := range comm.GroupBy(strategy) {
+		comm := &Commit{Changes: changes}
+		pR.Commits = append(pR.Commits, comm)
+	}
+	o.Merge(pR)
+	return
 }
+
+func (o *Owner) Merge(pR *PullRequest) {
+	for _, comm := range pR.Commits {
+	}
+}
+
+// ! A PR will be generated after a commit can be splitted into non-compatible changes
+// ! A PR will be splitted in subCommits
 
 // func (o *Owner) Pull(context.Context, *Commit) (*Commit, error) {
 // 	return nil
 // }
 
 // func (o *Owner) Push(context.Context, *Commit) (Summary, error) {
-
+// 	return
 // }
 
 // func (o *Owner) Delete(context.Context, *Commit) (Summary, error) {
-
+// 	return
 // }
