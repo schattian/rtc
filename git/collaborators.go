7ec5cd5630da57c1d7ec047222d4a107f21a46aa
sebach1 <sebachamena@gmail.com>
2019-10-08 22:51:32 -0300

    Edit core interface (Collaborator)

--- git/collaborators.go
+++ git/collaborators.go
@@ -4,11 +4,11 @@ import "context"
 
 // Collaborator is any agent which performs transactions
 type Collaborator interface {
-	Push(context.Context, *Commit) (Summary, error)
+	Push(context.Context, *Commit) (*Commit, error)
 
 	Pull(context.Context, *Commit) (*Commit, error)
 
-	Delete(context.Context, *Commit) (Summary, error)
+	Delete(context.Context, *Commit) (*Commit, error)
 
 	Init(context.Context) error
 }
