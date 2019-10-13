c8027653b6311a7ef0f7f64d1a8f82b62a5ef9fb
sebach1 <sebachamena@gmail.com>
2019-10-13 15:04:28 -0300

    Splits pR reviewing

--- git/owners.go
+++ git/owners.go
@@ -19,6 +19,51 @@ type Owner struct {
 	Summary chan *Result
 }
 
+func (own *Owner) ReviewPRCommit(sch *schema.Schema, pR *PullRequest, commIdx int) {
+	defer own.wg.Done()
+	comm := pR.Commits[commIdx]
+	defer func() {
+		if comm.Reviewer == nil {
+			comm.Errored = true
+		}
+	}()
+
+	schErrCh := make(chan error, len(comm.Changes))
+	for _, chg := range comm.Changes {
+		own.wg.Add(1)
+		go sch.Validate(chg.TableName, chg.ColumnName, chg.Options.Keys(), chg.Value(), own.Project, own.wg, schErrCh)
+	}
+
+	tableName, err := comm.TableName()
+	if err != nil {
+		own.Summary <- &Result{CommitID: comm.ID, Error: errors.Wrap(err, "revieweing pr")}
+		return
+	}
+
+	_, err = comm.Options()
+	if err != nil {
+		own.Summary <- &Result{CommitID: comm.ID, Error: errors.Wrap(err, "revieweing pr")}
+		return
+	}
+
+	if len(schErrCh) > 0 {
+		var errs string
+		for err := range schErrCh {
+			errs += err.Error()
+			errs += "; "
+		}
+		own.Summary <- &Result{CommitID: comm.ID, Error: errors.Wrap(err, "revieweing pr")}
+		return
+	}
+
+	reviewer, err := pR.Team.Delegate(tableName)
+	if err != nil {
+		own.Summary <- &Result{CommitID: comm.ID, Error: errors.Wrap(err, "revieweing pr")}
+		return
+	}
+	comm.Reviewer = reviewer
+}
+
 // Orchestrate sends the order to all the collaborators available to execute
 // the needed actions in order to achieve the commitment, creating a new PullRequest
 func (own *Owner) Orchestrate(
@@ -43,49 +88,33 @@ func (own *Owner) Orchestrate(
 	pR.AssignTeam(community, schName)
 
 	own.Summary = make(chan *Result, len(pR.Commits))
-	go own.Merge(ctx, &pR, sch)
+
+	own.wg.Add(len(pR.Commits))
+	for commIdx := range pR.Commits {
+		go own.ReviewPRCommit(sch, &pR, commIdx)
+	}
+	own.wg.Wait()
+
+	own.wg.Add(1)
+	go own.Merge(ctx, &pR)
 	own.wg.Wait()
 
 	return nil
 }
 
 // Merge performs the needed actions in order to merge the pullRequest
-func (own *Owner) Merge(ctx context.Context, pR *PullRequest, sch *schema.Schema) {
+func (own *Owner) Merge(ctx context.Context, pR *PullRequest) {
 	for _, comm := range pR.Commits {
+		if comm.Errored {
+			continue
+		}
+
 		commType, err := comm.Type()
 		if err != nil {
 			own.Summary <- &Result{CommitID: comm.ID, Error: errors.Wrap(err, "merging")}
-			continue // Discards the commit
-		}
-		tableName, err := comm.TableName()
-		if err != nil {
-			own.Summary <- &Result{CommitID: comm.ID, Error: errors.Wrap(err, "merging")}
-			continue // Discards the commit
+			continue
 		}
 
-		schErrCh := make(chan error, len(comm.Changes))
-		for _, colName := range comm.ColumnNames() {
-			own.wg.Add(1)
-			go sch.Validate(tableName, colName, own.Project, own.wg, schErrCh)
-		}
-
-		reviewer, err := pR.Team.Delegate(tableName)
-		if err != nil {
-			own.Summary <- &Result{CommitID: comm.ID, Error: errors.Wrap(err, "merging")}
-			continue // Discards the commit
-		}
-		comm.Reviewer = reviewer
-
-		own.wg.Wait()
-		if len(schErrCh) > 0 {
-			var errs string
-			for err := range schErrCh {
-				errs += err.Error()
-				errs += ";"
-			}
-			own.Summary <- &Result{CommitID: comm.ID, Error: errors.Wrap(errors.New(errs), "merging")}
-			continue // Discards the commit
-		}
 		switch commType {
 		case "create", "update":
 			own.wg.Add(1)
@@ -156,3 +185,12 @@ func (own *Owner) Delete(ctx context.Context, comm *Commit) (*Commit, error) {
 	*comm = *newComm
 	return comm, nil
 }
+
+// func (own *Owner) IsAlreadySummarized(commID int) bool {
+// 	for summ := range own.Summary {
+// 		if summ.CommitID == commID {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
