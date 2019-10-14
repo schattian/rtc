bc4945f94e98aaef9829a6b19d5be3e5c3937ee8
sebach1 <sebachamena@gmail.com>
2019-10-13 21:47:51 -0300

    Add noBlueprint err

--- schema/errors.go
+++ schema/errors.go
@@ -3,6 +3,9 @@ package schema
 import "errors"
 
 var (
+	// Schema errs
+	errNilBlueprint = errors.New("the BLUEPRINT is NIL")
+
 	// Table errs
 	errNonexistentTable = errors.New("the TABLE given does NOT EXISTS")
 	errForeignTable     = errors.New("the TABLE given does NOT BELONGS to the given SCHEMA")
