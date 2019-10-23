76155b87596382baf7f0f845a80de334e0cc3495
sebach1 <sebachamena@gmail.com>
2019-10-22 22:55:02 -0300

    Add fabric

---
+++ fabric.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/sebach1/git-crud/fabric"
+	"github.com/sebach1/git-crud/schema"
+	"github.com/urfave/cli"
+)
+
+var fabricCmd = cli.Command{
+	Name:        "fabric",
+	Aliases:     []string{"f"},
+	Description: "Create structs from the given schema tagging fields with the given marshal type",
+	Flags: []cli.Flag{
+		cli.StringFlag{Name: "marshal, msh"},
+	},
+	Usage:  "[schema-path]",
+	Before: fabricValidate,
+	Action: fabricExec,
+}
+
+func fabricExec(c *cli.Context) error {
+	schemaName := c.Args().First()
+	marshalType := c.String("marshal, msh")
+	if marshalType == "" {
+		marshalType = "json"
+	}
+
+	decodedSchema, err := schema.FromFilename(schemaName)
+	if err != nil {
+		panic(err)
+	}
+
+	Fabric := &fabric.Fabric{Schema: decodedSchema}
+	err = Fabric.Produce(marshalType)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func fabricValidate(c *cli.Context) error {
+	if !c.Args().Present() {
+		return errors.New("there are NOT ENOUGH ARGS present")
+	}
+	return nil
+}
--- fabric/fabric.go
+++ fabric/fabric.go
@@ -1,14 +1,13 @@
 package fabric
 
 import (
-	"bufio"
 	"bytes"
 	"errors"
 	"fmt"
 	"go/format"
 	"io/ioutil"
+	"log"
 	"os"
-	"regexp"
 	"strings"
 	"sync"
 
@@ -20,7 +19,7 @@ import (
 type Fabric struct {
 	Schema *schema.Schema
 
-	dir string
+	Dir string
 	wg  *sync.WaitGroup
 }
 
@@ -28,14 +27,16 @@ type Fabric struct {
 func (f *Fabric) Produce(marshal string) error {
 	err := f.validate()
 	if err != nil {
-		panic(err)
+		return err
 	}
-	f.dir = fmt.Sprintf("fabric/%v", f.Schema.Name)
-	err = os.MkdirAll(f.dir, os.ModePerm)
+	f.Schema.Name = integrity.SchemaName(strings.ToLower(string(f.Schema.Name)))
+	f.Dir = fmt.Sprintf("fabric/%v", f.Schema.Name)
+	err = os.MkdirAll(f.Dir, os.ModePerm)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
+	f.wg = new(sync.WaitGroup)
 	for _, table := range f.Schema.Blueprint {
 		f.wg.Add(1)
 		go f.writeStructFromTable(table, marshal)
@@ -48,24 +49,24 @@ func (f *Fabric) writeStructFromTable(table *schema.Table, marshal string) {
 	defer f.wg.Done()
 	var out bytes.Buffer
 	tableStruct := f.structFromTable(table, marshal)
-	structTemplate.Execute(bufio.NewWriter(&out), tableStruct)
-	filename := fmt.Sprintf("%v/%v.go", f.dir, strings.ToLower(string(table.Name)))
+	err := structTemplate.Execute(&out, tableStruct)
+	filename := fmt.Sprintf("%v/%v.go", f.Dir, strings.ToLower(string(table.Name)))
 	generated := out.Bytes()
-	generated, err := format.Source(generated)
+	generated, err = format.Source(generated)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	err = ioutil.WriteFile(filename, generated, os.ModePerm)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
 func (f *Fabric) structFromTable(table *schema.Table, marshal string) *tableData {
 	tableStruct := &tableData{
-		SchemaName: f.Schema.Name,
-		Name:       table.Name,
-		Marshal:    marshal,
+		SchemaName: string(f.Schema.Name),
+		Name:       toCamelCase(string(table.Name), true),
+		Marshal:    toSnakeCase(marshal, '_'),
 	}
 	for _, col := range table.Columns {
 		tableStruct.Fields = append(tableStruct.Fields, fieldFromColumn(col))
@@ -75,9 +76,9 @@ func (f *Fabric) structFromTable(table *schema.Table, marshal string) *tableData
 
 func fieldFromColumn(col *schema.Column) *columnData {
 	return &columnData{
-		Name: col.Name,
-		Type: col.Validator.NativeType(),
-		Tag:  toSnakeCase(string(col.Name)),
+		Name: toCamelCase(string(col.Name), true),
+		Type: col.Type,
+		Tag:  toSnakeCase(string(col.Name), '_'),
 	}
 }
 
@@ -91,23 +92,15 @@ func (f *Fabric) validate() error {
 	return nil
 }
 
-func toSnakeCase(str string) string {
-	matchFirstCap := regexp.MustCompile("(.)([A-Z][a-z]+)")
-	matchAllCap := regexp.MustCompile("([a-z0-9])([A-Z])")
-	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
-	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
-	return strings.ToLower(snake)
-}
-
 type tableData struct {
-	SchemaName integrity.SchemaName
-	Name       integrity.TableName
+	SchemaName string
+	Name       string
 	Fields     []*columnData
 	Marshal    string
 }
 
 type columnData struct {
-	Name integrity.ColumnName
+	Name string
 	Type integrity.ValueType
 	Tag  string
 }
