0c4094341eed5b8b17407fe820a259adeb8c32c6
sebach1 <sebachamena@gmail.com>
2019-10-22 22:57:36 -0300

    Correct fabric template

--- fabric/struct.go
+++ fabric/struct.go
@@ -7,8 +7,9 @@ package {{.SchemaName}}
 
 // {{.Name}} is the native representation of the {{.Name}} resource in {{.SchemaName}} schema
 type {{.Name}} struct {
+	{{$msh := .Marshal}}
 	{{range $field := .Fields}}
-		{{$field.Name}}   {{$field.Type}}   ` + "`{{.Marshal}}:\"{{$field.Tag}}\"`" + `
+		{{$field.Name}}   {{$field.Type}}   ` + "`{{$msh}}:\"{{$field.Tag}}\"`" + `
 	{{end}}
 }
 `
