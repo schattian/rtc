3b6a4256a204ff916088b7ab0ead5dfc0f62d50f
Sebastián Chamena <sebachamena@gmail.com>
2019-10-27 11:16:19 -0300

    Add selfValidation to table

--- schema/table.go
+++ schema/table.go
@@ -1,6 +1,8 @@
 package schema
 
 import (
+	"sync"
+
 	"github.com/sebach1/git-crud/integrity"
 )
 
@@ -9,6 +11,58 @@ type Table struct {
 	Name       integrity.TableName   `json:"name,omitempty"`
 	Columns    []*Column             `json:"columns,omitempty"`
 	OptionKeys []integrity.OptionKey `json:"option_keys,omitempty"`
+}
+
+func (t *Table) validateSelf(wg *sync.WaitGroup, vErrCh chan<- error) {
+	defer wg.Done()
+	if t == nil {
+		vErrCh <- t.validationErr(errNilTable)
+		return
+	}
+
+	colsQt := len(t.Columns)
+	if colsQt == 0 {
+		vErrCh <- t.validationErr(errNilColumns)
+	}
+
+	var tVWg sync.WaitGroup
+	tVWg.Add(colsQt)
+	cVErrCh := make(chan error, colsQt)
+	for _, col := range t.Columns {
+		go col.validateSelf(&tVWg, cVErrCh)
+	}
+
+	if t.Name == "" {
+		vErrCh <- t.validationErr(errNilTableName)
+	}
+
+	tVWg.Wait()
+	close(cVErrCh)
+	for err := range cVErrCh {
+		vErrCh <- err
+	}
+}
+
+func (t *Table) validationErr(err error) *integrity.ValidationError {
+	var name string
+	if t == nil {
+		name = ""
+	} else {
+		name = string(t.Name)
+	}
+	return &integrity.ValidationError{Err: err, Origin: "table", OriginName: name}
+}
+
+// Copy returns a copy of the given table, including embedded cols
+func (t *Table) Copy() *Table {
+	newTab := new(Table)
+	*newTab = *t
+	var newCols []*Column
+	for _, col := range newTab.Columns {
+		newCols = append(newCols, col.Copy())
+	}
+	newTab.Columns = newCols
+	return newTab
 }
 
 func (t *Table) columnNames() (colNames []integrity.ColumnName) {
