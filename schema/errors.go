821ab81651c7a5eca2645a307f84bc2d96447869
Sebastián Chamena <sebachamena@gmail.com>
2019-10-27 11:16:34 -0300

    Add singular validationErrs to schema pkg

--- schema/errors.go
+++ schema/errors.go
@@ -4,14 +4,22 @@ import "errors"
 
 var (
 	// Schema errs
-	errNilBlueprint = errors.New("the BLUEPRINT is NIL")
+	errNilBlueprint  = errors.New("the BLUEPRINT is NIL")
+	errNilSchema     = errors.New("the SCHEMA cannot be NIL")
+	errNilSchemaName = errors.New("the SCHEMA NAME cannot be NIL")
 
 	// Table errs
 	errNonexistentTable = errors.New("the TABLE given does NOT EXISTS")
 	errForeignTable     = errors.New("the TABLE given does NOT BELONGS to the given SCHEMA")
 	errInvalidOptionKey = errors.New("the provided OPTION KEY is INVALID OR NOT DEFINED")
+	errNilTableName     = errors.New("the TABLE NAME is NIL")
+	errNilColumns       = errors.New("the COLUMNS cannot be NIL")
+	errNilTable         = errors.New("the TABLE is NIL")
 
 	// Column errs
 	errNonexistentColumn = errors.New("the COLUMN given does NOT EXISTS")
 	errForeignColumn     = errors.New("the COLUMNS given does NOT BELONGS to the given TABLE")
+	errNilColumnName     = errors.New("the COLUMN NAME is NIL")
+	errNilColumn         = errors.New("the COLUMN is NIL")
+	errNilColumnType     = errors.New("the COLUMN TYPE is NIL")
 )
