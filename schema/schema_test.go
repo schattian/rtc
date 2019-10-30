801f1e45c0932f0528fd59c5fb59b17cb420b33e
sebach1 <sebachamena@gmail.com>
2019-10-29 22:50:24 -0300

    Add validateSelf tests

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -1,6 +1,7 @@
 package schema
 
 import (
+	"strconv"
 	"sync"
 	"testing"
 
@@ -137,4 +138,133 @@ func TestSchema_Validate(t *testing.T) {
 			}
 		})
 	}
+}
+
+type schemaVary struct {
+	name     string
+	function func(sch *Schema) *Schema
+	err      error
+}
+
+func TestSchema_WrapValidateSelf(t *testing.T) {
+	t.Parallel()
+	fuzzyTests := []*schemaVary{
+		// Schema
+		&schemaVary{
+			name:     "sch nil name",
+			function: func(sch *Schema) *Schema { sch.Name = ""; return sch },
+			err:      errNilSchemaName},
+		// Table
+		&schemaVary{
+			name:     "table nil name",
+			function: func(sch *Schema) *Schema { sch.Blueprint[0].Name = ""; return sch },
+			err:      errNilTableName},
+		// Column
+		&schemaVary{
+			name:     "col nil type",
+			function: func(sch *Schema) *Schema { sch.Blueprint[0].Columns[0].Type = ""; return sch },
+			err:      errNilColumnType},
+		&schemaVary{
+			name:     "col nil name",
+			function: func(sch *Schema) *Schema { sch.Blueprint[0].Columns[0].Name = ""; return sch },
+			err:      errNilColumnName},
+	}
+	normalTests := []*schemaVary{
+		// Schema
+		&schemaVary{
+			name:     "sch nil",
+			function: func(sch *Schema) *Schema { return nil },
+			err:      errNilSchema},
+		&schemaVary{
+			name:     "sch nil bp",
+			function: func(sch *Schema) *Schema { sch.Blueprint = nil; return sch },
+			err:      errNilBlueprint},
+		// Table
+		&schemaVary{
+			name:     "table nil",
+			function: func(sch *Schema) *Schema { sch.Blueprint[0] = nil; return sch },
+			err:      errNilTable},
+		&schemaVary{
+			name:     "table nil columns",
+			function: func(sch *Schema) *Schema { sch.Blueprint[0].Columns = nil; return sch },
+			err:      errNilColumns},
+		// Column
+		&schemaVary{
+			name:     "col nil",
+			function: func(sch *Schema) *Schema { sch.Blueprint[0].Columns[0] = nil; return sch },
+			err:      errNilColumn},
+	}
+	for _, tt := range normalTests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			sch := tt.function(gSchemas.Basic.Copy())
+			err := sch.WrapValidateSelf()
+			unwrappedVErr := integrity.UnwrapValidationError(err, false)
+			diffGot, diffWant := diffBetweenErrs(unwrappedVErr, []error{tt.err})
+			if diffGot != nil {
+				t.Errorf("Schema.WrapValidate() GOT more errs than want: %v", diffGot)
+			}
+			if diffWant != nil {
+				t.Errorf("Schema.WrapValidate() WANT more errs than got: %v", diffWant)
+			}
+		})
+	}
+	for k, test := range fuzzyFactorial(fuzzyTests) {
+		test := test
+		var wantErrs []error
+		sch := gSchemas.Basic.Copy()
+		for _, tt := range test {
+			sch = tt.function(sch)
+			wantErrs = append(wantErrs, tt.err)
+		}
+		t.Run(strconv.Itoa(k), func(t *testing.T) {
+			t.Parallel()
+			err := sch.WrapValidateSelf()
+			unwrappedVErr := integrity.UnwrapValidationError(err, false)
+			diffGot, diffWant := diffBetweenErrs(unwrappedVErr, wantErrs)
+			if diffGot != nil {
+				t.Errorf("Schema.WrapValidate() GOT more errs than want: %v", diffGot)
+			}
+			if diffWant != nil {
+				t.Errorf("Schema.WrapValidate() WANT more errs than got: %v", diffWant)
+			}
+		})
+	}
+}
+func fuzzyFactorial(set []*schemaVary) (subsets [][]*schemaVary) {
+	length := uint(len(set))
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		var subset []*schemaVary
+		for object := uint(0); object < length; object++ {
+			if (subsetBits>>object)&1 == 1 {
+				subset = append(subset, set[object])
+			}
+		}
+		subsets = append(subsets, subset)
+	}
+	return subsets
+}
+
+func strErrIsInArray(err error, errs []error) bool {
+	for _, otherErr := range errs {
+		if otherErr.Error() == err.Error() {
+			return true
+		}
+	}
+	return false
+}
+
+func diffBetweenErrs(errs []error, in []error) (errsOnly []error, inOnly []error) {
+	for _, err := range errs {
+		if !strErrIsInArray(err, in) {
+			errsOnly = append(errsOnly, err)
+		}
+	}
+	for _, otherErr := range in {
+		if !strErrIsInArray(otherErr, errs) {
+			inOnly = append(inOnly, otherErr)
+		}
+	}
+	return
 }
