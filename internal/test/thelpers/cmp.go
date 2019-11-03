4e8c6bc3a29ef8d6fbc54c73412f798001eafd4d
sebach1 <sebachamena@gmail.com>
2019-11-03 16:22:25 -0300

    Add CmpWithGoldenFile thelper

--- internal/test/thelpers/cmp.go
+++ internal/test/thelpers/cmp.go
@@ -1,6 +1,8 @@
 package thelpers
 
 import (
+	"fmt"
+	"io/ioutil"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -9,11 +11,24 @@ import (
 func CmpIfErr(t *testing.T, err error, old, actual, new interface{}, msg string) {
 	if err != nil {
 		if diff := cmp.Diff(old, actual); diff != "" {
-			t.Errorf("%v errored mismatch (-want +got): %s", msg, diff)
+			t.Errorf("%s errored mismatch (-want +got): %s", msg, diff)
 		}
 		return
 	}
 	if diff := cmp.Diff(new, actual); diff != "" {
-		t.Errorf("%v mismatch (-want +got): %s", msg, diff)
+		t.Errorf("%s mismatch (-want +got): %s", msg, diff)
+	}
+}
+
+func CmpWithGoldenFile(t *testing.T, got []byte, goldenFilename, msg string) {
+	t.Helper()
+
+	want, err := ioutil.ReadFile(fmt.Sprintf("testdata/%s", goldenFilename))
+	if err != nil {
+		t.Fatalf("Error trying to read the goldenFile: %s", err)
+	}
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("%s mismatch (-want +got): %s", msg, diff)
 	}
 }
