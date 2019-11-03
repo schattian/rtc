f12620b595e8bf7807db28ebb44ca3e63d0eba6f
sebach1 <sebachamena@gmail.com>
2019-11-03 16:21:50 -0300

    Supress undesired newline on template

--- fabric/struct.go
+++ fabric/struct.go
@@ -8,9 +8,9 @@ package {{.SchemaName}}
 // {{.Name}} is the native representation of the {{.Name}} resource in {{.SchemaName}} schema
 type {{.Name}} struct {
 	{{$msh := .Marshal}}
-	{{range $field := .Fields}}
-		{{$field.Name}}   {{$field.Type}}   ` + "`{{$msh}}:\"{{$field.Tag}}\"`" + `
-	{{end}}
+	{{- range $field := .Fields}}
+		{{$field.Name}} {{$field.Type}} ` + "`{{$msh}}:\"{{$field.Tag}}\"`" + `
+	{{- end}}
 }
 `
 
