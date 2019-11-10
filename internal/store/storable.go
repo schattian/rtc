22b36f024d0d6473aa4ef0d3c25790a42f8c0af0
sebach1 <sebachamena@gmail.com>
2019-11-10 15:13:31 -0300

    Add storable interface and methods

---
+++ internal/store/storable.go
@@ -0,0 +1,48 @@
+package store
+
+import (
+	"context"
+	"strings"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/sebach1/git-crud/internal/name"
+
+	"github.com/sebach1/git-crud/internal/field"
+)
+
+type Storable interface {
+	field.Store
+	SetID(int64)
+	Table() string
+	Columns() []string
+}
+
+func sqlColumnValues(storable Storable) string {
+	fValues := []string{}
+	for _, s := range storable.Columns() {
+		fValues = append(fValues, ":"+s)
+	}
+	return name.Parenthize(strings.Join(fValues, ","))
+}
+
+func sqlColumnNames(storable Storable) string {
+	return name.Parenthize(strings.Join(storable.Columns(), ","))
+}
+
+func SaveToDB(storable Storable, ctx context.Context, db *sqlx.DB) error {
+	res, err := db.NamedExecContext(
+		ctx,
+		`INSERT INTO`+storable.Table()+` `+sqlColumnNames(storable)+` VALUES `+sqlColumnValues(storable),
+		storable,
+	)
+	if err != nil {
+		return err
+	}
+	possibleID, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+	storable.SetID(possibleID)
+	return nil
+
+}
