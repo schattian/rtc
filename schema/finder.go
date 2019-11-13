3a05414faa0a941f8403d77ea77b2f4bb3b96bbd
Sebastián Chamena <sebachamena@gmail.com>
2019-11-12 21:44:15 -0300

    Add comments to schema finder

--- schema/finder.go
+++ schema/finder.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sebach1/git-crud/integrity"
 )
 
+// ByName finds a schema from the DB given its name
 func ByName(ctx context.Context, db *sqlx.DB, schName integrity.SchemaName) (*Schema, error) {
 	sch := Schema{}
 	err := db.GetContext(ctx, &sch, `SELECT * FROM schemas WHERE name=?`, schName)
