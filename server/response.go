16d8c1d170c561d48db7e3c3ac72085d454b24b1
Sebastián Chamena <sebachamena@gmail.com>
2020-01-14 14:41:17 -0300

    Add resp body & err

---
+++ server/response.go
@@ -0,0 +1,14 @@
+package server
+
+import "github.com/sebach1/rtc/git"
+
+type respBody struct {
+	Commit      *git.Commit
+	Commits     []*git.Commit
+	Change      *git.Change
+	PullRequest *git.PullRequest
+}
+
+type respBodyErr struct {
+	Err error
+}
