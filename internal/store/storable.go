2aa636bf7b5e12e99480aa8edf1d8f9aa0148d0f
Sebastián Chamena <sebachamena@gmail.com>
2020-01-14 14:40:26 -0300

    Improve storable methods (due tests)

--- internal/store/storable.go
+++ internal/store/storable.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"context"
+	"errors"
 	"strings"
 
 	"github.com/jmoiron/sqlx"
@@ -28,34 +29,64 @@ func sqlColumnNames(storable Storable) string {
 	return name.Parenthize(strings.Join(storable.SQLColumns(), ","))
 }
 
-func UpdateIntoDB(ctx context.Context, db *sqlx.DB, storable Storable) error {
-	if ctx == nil {
-		ctx = context.Background()
+func UpsertIntoDB(ctx context.Context, db *sqlx.DB, storables ...Storable) error {
+	var inserts, updates []Storable
+	for _, store := range storables {
+		if store.GetId() == 0 {
+			inserts = append(inserts, store)
+		} else {
+			updates = append(updates, store)
+		}
 	}
-	_, err := db.NamedExecContext(ctx, execBoilerplate("UPDATE", storable), storable)
-	return err
-}
-
-func UpdateBatchIntoDB(ctx context.Context, db *sqlx.DB, storables ...Storable) error {
-	if ctx == nil {
-		ctx = context.Background()
-	}
-	ref := storables[0]
-	_, err := db.NamedExecContext(ctx, execBoilerplate("UPDATE", ref), storables)
-	return err
-}
-
-// InsertIntoDB inserts the storable entity to the DB
-// Finally, it assigns the inserted Id to the given entity
-func InsertIntoDB(ctx context.Context, db *sqlx.DB, storable Storable) error {
-	if ctx == nil {
-		ctx = context.Background()
-	}
-	res, err := db.NamedExecContext(ctx, execBoilerplate("INSERT INTO", storable), storable)
+	err := UpdateIntoDB(ctx, db, updates...)
 	if err != nil {
 		return err
 	}
-	id, err := res.LastInsertId()
+	// err = InsertIntoDB(ctx, db, inserts...)
+	// if err != nil {
+	// 	return err
+	// }
+	return nil
+}
+
+func UpdateIntoDB(ctx context.Context, db *sqlx.DB, storables ...Storable) error {
+	if len(storables) == 0 {
+		return nil
+	}
+
+	ref := storables[0] // takes it as a reference for all entities given
+	qr := execBoilerplate("UPDATE", ref)
+	res, err := db.NamedExecContext(ctx, qr, storables)
+	if err != nil {
+		return err
+	}
+	rowsAff, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAff != int64(len(storables)) {
+		return errMismatchAffectedRows
+	}
+	return nil
+}
+
+var errNilStorableEntity = errors.New("nil storable entity")
+var errMismatchAffectedRows = errors.New("the affected rows quantity does not match with the given storables")
+
+// InsertIntoDB inserts the storable entity to the DB
+// Finally, it assigns the inserted Id to the given entity
+func InsertIntoDB(ctx context.Context, db *sqlx.DB, storables ...Storable) error {
+	if len(storables) == 0 {
+		return errNilStorableEntity
+	}
+	ref := storables[0] // takes it as a reference for all entities given
+	qr := execBoilerplate("INSERT INTO", ref) + " RETURNING id"
+	row, err := db.NamedExecContext(ctx, qr, storables)
+	if err != nil {
+		return err
+	}
+
+	id, err := row.LastInsertId()
 	if err != nil {
 		return err
 	}
@@ -78,15 +109,6 @@ func DeleteFromDB(ctx context.Context, db *sqlx.DB, storable Storable) error {
 	)
 	return err
 }
-
-// func containsColumn(storable Storable, columnName string) bool{
-// 	for _, col := range storable.SQLColumns(){
-// 		if col == columnName{
-// 			return true
-// 		}
-// 	}
-// 			return false
-// }
 
 func execBoilerplate(action string, storable Storable) string {
 	return action + ` ` + storable.SQLTable() + ` ` + sqlColumnNames(storable) + ` VALUES ` + sqlColumnValues(storable)
